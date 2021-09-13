7ab51a20e177fc40d19fe2951c2aab1af7278d89
ajaybarath <ajaybarath3@gmail.com>
2021-09-13 16:47:18 +0530

    ADDED: method to analyse the mood

--- src/main/java/com/bridgeLabz/moodAnalyzer/MoodAnalyser.java
+++ src/main/java/com/bridgeLabz/moodAnalyzer/MoodAnalyser.java
@@ -1,5 +1,17 @@
 package com.bridgeLabz.moodAnalyzer;
 
 public class MoodAnalyser {
+	
+	String message = "I am happy right now";
+	
+	public String analiseMood() {
+		if (message.contains("Sad")) {
+			return "SAD";
+		}
+		else if (message.contains("Happy")){
+			return "HAPPY";
+		}
+		return null;
+	}
 
 }
