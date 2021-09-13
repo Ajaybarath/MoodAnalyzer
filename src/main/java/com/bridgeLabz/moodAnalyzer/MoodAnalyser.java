c00fea4acda470958b1b3c0b72755da194ee7684
ajaybarath <ajaybarath3@gmail.com>
2021-09-13 16:55:43 +0530

    REFRACTORED: analyseMood function to return happy if not sad

--- src/main/java/com/bridgeLabz/moodAnalyzer/MoodAnalyser.java
+++ src/main/java/com/bridgeLabz/moodAnalyzer/MoodAnalyser.java
@@ -1,22 +1,20 @@
 package com.bridgeLabz.moodAnalyzer;
 
 public class MoodAnalyser {
-	
+
 	String message = "I am happy right now";
-	
+
 	public MoodAnalyser(String message) {
 		// TODO Auto-generated constructor stub
 		this.message = message;
 	}
-	
+
 	public String analiseMood() {
 		if (message.contains("Sad")) {
 			return "SAD";
 		}
-		else if (message.contains("Happy")){
-			return "HAPPY";
-		}
-		return null;
+
+		return "HAPPY";
 	}
 
 }
