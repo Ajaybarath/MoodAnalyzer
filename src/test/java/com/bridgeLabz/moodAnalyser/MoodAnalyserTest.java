29facb81dc1ebd83e8942250f25235e39296437c
ajaybarath <ajaybarath3@gmail.com>
2021-09-13 17:53:00 +0530

    ADDED: test function to handle custom null exception

--- src/test/java/com/bridgeLabz/moodAnalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgeLabz/moodAnalyser/MoodAnalyserTest.java
@@ -3,39 +3,54 @@ package com.bridgeLabz.moodAnalyser;
 import org.junit.Test;
 
 import com.bridgeLabz.moodAnalyzer.MoodAnalyser;
+import com.bridgeLabz.moodAnalyzer.MoodAnalysisException;
+import com.bridgeLabz.moodAnalyzer.MoodAnalysisException.ExceptionType;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 public class MoodAnalyserTest {
 	
 	@Test
-	public void analyseSadTestCase() {
+	public void analyseSadTestCase() throws MoodAnalysisException {
 		
 		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
 		
-		String moodString = moodAnalyser.analiseMood();
+		String moodString = moodAnalyser.analyseMood();
 		
 		Assert.assertEquals("SAD", moodString);
 	}
 	
 	@Test
-	public void analyseAnyMoodTestCase() {
+	public void analyseAnyMoodTestCase() throws MoodAnalysisException {
 		
 		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any Mood");
 		
-		String moodString = moodAnalyser.analiseMood();
+		String moodString = moodAnalyser.analyseMood();
 		
 		Assert.assertEquals("HAPPY", moodString);
 	}
 	
 	@Test
-	public void handleNullMoodTestCase() {
+	public void handleNullMoodTestCase() throws MoodAnalysisException {
 		
 		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
 		
-		String moodString = moodAnalyser.analiseMood();
+		String moodString = moodAnalyser.analyseMood();
 		
-		Assert.assertEquals("HAPPY", moodString);
+		Assert.assertEquals("Please enter some data in the string", moodString);
+	}
+	
+	@Test
+	public void nullMoodThrowExceptionTestCase() {
+		
+		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+		try {
+			moodAnalyser.analyseMood();
+		}
+		catch (MoodAnalysisException e) {
+
+			Assert.assertEquals(MoodAnalysisException.ExceptionType.NULL, e.type);
+		}
 	}
 
 }
