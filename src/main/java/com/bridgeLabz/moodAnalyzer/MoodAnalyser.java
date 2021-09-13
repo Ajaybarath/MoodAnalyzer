848ed522d5542346e5a5bd2c51673f3be735c6d7
ajaybarath <ajaybarath3@gmail.com>
2021-09-13 16:53:40 +0530

    ADDED: constructor to get the message data

--- src/main/java/com/bridgeLabz/moodAnalyzer/MoodAnalyser.java
+++ src/main/java/com/bridgeLabz/moodAnalyzer/MoodAnalyser.java
@@ -4,6 +4,11 @@ public class MoodAnalyser {
 	
 	String message = "I am happy right now";
 	
+	public MoodAnalyser(String message) {
+		// TODO Auto-generated constructor stub
+		this.message = message;
+	}
+	
 	public String analiseMood() {
 		if (message.contains("Sad")) {
 			return "SAD";
