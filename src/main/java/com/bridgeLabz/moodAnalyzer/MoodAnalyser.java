45309996576bff0cd14ce72aee84b327c428c989
ajaybarath <ajaybarath3@gmail.com>
2021-09-13 17:52:17 +0530

    REFRACTORED: with custom exception for handling null and empty message

--- src/main/java/com/bridgeLabz/moodAnalyzer/MoodAnalyser.java
+++ src/main/java/com/bridgeLabz/moodAnalyzer/MoodAnalyser.java
@@ -1,22 +1,29 @@
 package com.bridgeLabz.moodAnalyzer;
 
+import com.bridgeLabz.moodAnalyzer.MoodAnalysisException.ExceptionType;
+
 public class MoodAnalyser {
 
-	String message = "I am happy right now";
+	String message = "";
 
 	public MoodAnalyser(String message) {
 		// TODO Auto-generated constructor stub
 		this.message = message;
 	}
 
-	public String analiseMood() {
+	public String analyseMood() throws MoodAnalysisException {
 		try {
+			if (message.length() == 0) {
+				throw new MoodAnalysisException("Please enter some data in the string", ExceptionType.EMPTY);
+			}
+			
 			if (message.contains("Sad")) {
 				return "SAD";
 			}
 
-		} catch (Exception e) {
-			// TODO: handle exception
+		} catch (NullPointerException e) {
+			throw new MoodAnalysisException("Please enter some data in the string", ExceptionType.NULL);
+
 		}
 		
 		return "HAPPY";
